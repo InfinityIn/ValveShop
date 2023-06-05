ecd197849e608a7538c0c5a11746b001d1fa9429
Максим Девякович <mdevyakovich@intelkon.com>
2023-06-05 18:02:22 +0300

    git init

---
+++ webapi/Models/MainMenuItem.cs
@@ -0,0 +1,8 @@
+﻿namespace webapi.Models
+{
+  public class MainMenuItem
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
---
+++ webapi/Models/ProductProperty.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+
+namespace webapi.Models
+{
+  public class ProductProperty
+  {
+    public ProductProperty(int _id, string _name, int _priority) 
+    {
+      Id = _id;
+      Name = _name;
+      Priority = _priority;
+    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Priority { get; set; }
+  }
+
+  public class CheckBoxProductProperty : ProductProperty
+  {
+    public CheckBoxProductProperty(int _id, string _name, int _priority, bool _defaultValue) : base(_id, _name, _priority)
+    {
+      DefaultValue = _defaultValue;
+    }
+    public bool DefaultValue { get; set; }
+  }
+
+  public class RadioGroupProductProperty : ProductProperty
+  {
+    public RadioGroupProductProperty(int _id, string _name, int _priority, int _defaultValue, IReadOnlyDictionary<string, int> _values) 
+      : base(_id, _name, _priority)
+    {
+      DefaultValue = _defaultValue;
+      Values = _values.ToImmutableDictionary();
+    }
+    public int DefaultValue { get; set; }
+    public ImmutableDictionary<string, int> Values { get; set; }
+
+  }
+}
+ 
