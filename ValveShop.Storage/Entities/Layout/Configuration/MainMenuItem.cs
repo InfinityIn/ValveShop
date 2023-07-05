57503afb57fc6ae517ee9e7b426c45448e9b4093
InfinityIn <smailmc68@gmail.com>
2023-07-05 19:56:57 +0300

    refactoring

--- ValveShop.Storage/Entities/Layout/Configuration/MainMenuItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webapi.Models
-{
-  public class MainMenuItem
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-  }
-}
--- ValveShop.Storage/Entities/Layout/FollowedMenuItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webapi.Models
-{
-  public class FollowedMenuItem
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int MainMenuItemId { get; set; }
-  }
-}
